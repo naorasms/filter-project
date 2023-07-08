bc36ba8be9f66dd73db2139c06f79ff43dfa647c
naorasms <naorams@gmail.com>
2023-07-08 11:03:15 +0300

    first commeit

---
+++ app.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        id:1,
+        name: "Dress",
+        img: "img/dress2.jpg",
+        price : 50,
+        cat: "Dress",
+    },
+    {
+        id:2,
+        name: "Dress",
+        img: "img/dress.jpg",
+        price : 50,
+        cat: "Dress",
+    },
+    {
+        id:3,
+        name: "Luxury",
+        img: "img/Luxury2.jpg",
+        price : 50,
+        cat: "Luxury",
+    },
+    {
+        id:4,
+        name: "sport",
+        img: "img/sport.jpg",
+        price : 50,
+        cat: "Sport",
+    },
+    {
+        id:5,
+        name: "text",
+        img: "img/luxury.jpg",
+        price : 50,
+        cat: "luxury",
+    },
+    {
+        id:6,
+        name: "text",
+        img: "img/casual.jpg",
+        price : 50,
+        cat: "Casual",
+    },
+]
